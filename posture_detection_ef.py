13ec7e55ebc48d483444b1e461d165e9e4815c6f
sam9tw <sam9tw@gmail.com>
2025-07-08 17:34:50 +0800

    Add files via upload

--- posture_detection_ef.py
+++ posture_detection_ef.py
@@ -15,15 +15,40 @@ def get_angle(a, b, c):
     angle = np.arccos(np.clip(cosine, -1.0, 1.0))
     return np.degrees(angle)
 
-def shoulder_abduction(shoulder, elbow, wrist, hip, step=-1, round=0, start=False, start_time=0):
+def is_arm_straight(shoulder, elbow, wrist, tolerance=0.1):
+    a = np.linalg.norm(np.array(shoulder) - np.array(elbow))
+    b = np.linalg.norm(np.array(elbow) - np.array(wrist))
+    c = np.linalg.norm(np.array(shoulder) - np.array(wrist))
+    return abs((a + b) - c) < tolerance
+
+def shoulder_abduction(img, shoulder, elbow, wrist, hip, step=-1, round=0, start=False, start_time=0):
+    """
+    step: -1: not started, 0: start, 1: raising arms, 2: middle, 3: lowering arms
+    round: number of completed rounds
+    start: whether the action has started
+    start_time: time when the action started
+    """
     angle_shoulder = get_angle(hip, shoulder, elbow)
-    angle_elbow = get_angle(shoulder, elbow, wrist)
+    # angle_elbow = get_angle(shoulder, elbow, wrist)
     current_time = time.time()
 
-    if not (160 < angle_elbow < 200):
+    if not is_arm_straight(shoulder, elbow, wrist):
         print("請伸直手臂")
         cv2.putText(img, "Please stretch your arms", (int(w/3), h-30), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
         return step, round, start, start_time
+    
+    # max_horizontal_diff = 0.2  
+    # horizontal_diff = abs(wrist[0] - shoulder[0])
+    max_depth_diff = 0.07
+    # Considering the difference of arm's length for each person, using depth instead of horizontal distance, 
+    # hip is used as the reference point to calculate the depth difference
+    depth_diff = abs((wrist[2]-hip[2]) - (shoulder[2]-hip[2])) 
+    if depth_diff > max_depth_diff:
+        # print("請側舉而非直舉: ", horizontal_diff, " ", depth_diff, " ", wrist[2], " ", shoulder[2])
+        cv2.putText(img, "Not a shoulder abduction", (int(w/3), h-30), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
+        # step, start, start_time = -1, False, 0
+        return step, round, start, start_time
+    # print(horizontal_diff, " ", depth_diff, " ", wrist[2], " ", shoulder[2])
 
     if step == -1 and 15 < angle_shoulder < 25:
         step, start_time, start = 0, current_time, True
@@ -37,7 +62,7 @@ def shoulder_abduction(shoulder, elbow, wrist, hip, step=-1, round=0, start=Fals
             cv2.putText(img, "Please raise your hands slowly", (int(w/3), h-60), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
             print("請慢慢抬起手")
 
-    elif step == 1 and 170 < angle_shoulder < 190 and start:
+    elif step == 1 and 160 < angle_shoulder < 180 and start:
         if current_time - start_time > 2:
             step, start_time = 2, current_time
         else:
@@ -51,7 +76,7 @@ def shoulder_abduction(shoulder, elbow, wrist, hip, step=-1, round=0, start=Fals
             cv2.putText(img, "Please lower your arms slowly.", (int(w/3), h-60), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
             print("請慢慢放下手臂")
 
-    elif step == 3 and 5 < angle_shoulder < 15 and start:
+    elif step == 3 and 5 < angle_shoulder < 20 and start:
         if current_time - start_time > 2:
             round += 1
             step, start = -1, False
@@ -107,7 +132,7 @@ with mp_pose.Pose(
 
             h, w, _ = img.shape
             landmarks = results.pose_landmarks.landmark
-
+            # print(img.shape)
             pose_dict = {}
             for i, lm in enumerate(landmarks):
                 name = mp_pose.PoseLandmark(i).name
@@ -118,7 +143,7 @@ with mp_pose.Pose(
             #X:[0] Y:[1] Z:[2] 
             
             left_shoulder_abduction_step, left_shoulder_abduction_round, left_shoulder_abduction_start, left_shoulder_abduction_start_time = \
-                shoulder_abduction(pose_dict["LEFT_SHOULDER"], pose_dict["LEFT_ELBOW"], pose_dict["LEFT_WRIST"], pose_dict["LEFT_HIP"],
+                shoulder_abduction(img, pose_dict["LEFT_SHOULDER"], pose_dict["LEFT_ELBOW"], pose_dict["LEFT_WRIST"], pose_dict["LEFT_HIP"],
                 left_shoulder_abduction_step, left_shoulder_abduction_round, left_shoulder_abduction_start, left_shoulder_abduction_start_time)
             cv2.putText(img, f'Left shoulder abduction:', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
             cv2.putText(img, f'Step: {left_shoulder_abduction_step}', (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
@@ -127,7 +152,7 @@ with mp_pose.Pose(
             cv2.putText(img, f'Angle(180): {get_angle(pose_dict["LEFT_SHOULDER"], pose_dict["LEFT_ELBOW"], pose_dict["LEFT_WRIST"])}', (10, 150), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
 
             right_shoulder_abduction_step, right_shoulder_abduction_round, right_shoulder_abduction_start, right_shoulder_abduction_start_time = \
-                shoulder_abduction(pose_dict["RIGHT_SHOULDER"], pose_dict["RIGHT_ELBOW"], pose_dict["RIGHT_WRIST"], pose_dict["RIGHT_HIP"],
+                shoulder_abduction(img, pose_dict["RIGHT_SHOULDER"], pose_dict["RIGHT_ELBOW"], pose_dict["RIGHT_WRIST"], pose_dict["RIGHT_HIP"],
                 right_shoulder_abduction_step, right_shoulder_abduction_round, right_shoulder_abduction_start, right_shoulder_abduction_start_time)
             cv2.putText(img, f'Right shoulder abduction:', (0, 180), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 0), 2)
             cv2.putText(img, f'Step: {right_shoulder_abduction_step}', (0, 210), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 0), 2)
